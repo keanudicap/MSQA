48b1b950240db7e3d64787b1fa5132fd9a0d30f3
XM-WANG <hnu.wzz@gmail.com>
2023-05-30 03:14:41 +0000

    change an example for better viz

--- viz.py
+++ viz.py
@@ -4,7 +4,7 @@ import pandas as pd
 def foo(text1, text2, save_path):
     text1 = text1.splitlines()
     text2 = text2.splitlines()
-    diff = difflib.HtmlDiff(wrapcolumn=100)
+    diff = difflib.HtmlDiff(wrapcolumn=60)
     result = diff.make_file(text1, text2)
     with open(save_path,"w") as f:
         f.write(result)
@@ -14,7 +14,7 @@ def foo(text1, text2, save_path):
 if __name__ == "__main__":
     data_path = "./msqa-p-45k.csv"
     save_path = './viz_demo.html'
-    sample_idx = 26
+    sample_idx = 42
 
     df = pd.read_csv(data_path)
     answer_raw = df.AnswerText.iloc[sample_idx]
